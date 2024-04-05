cf59ca337a5917bd9c468cb3a7e9299fcaa776a2
ankiPonyash <ponyash_dragon@mail.ru>
2024-04-06 08:37:24 +1000

    Cliker?

--- Lanfi/Lanfi/MainPage.xaml.cs
+++ Lanfi/Lanfi/MainPage.xaml.cs
@@ -10,9 +10,61 @@ namespace Lanfi
 {
     public partial class MainPage : ContentPage
     {
+        int BoxValue = 0;
+        Label label = new Label
+        {
+            Text = "0",
+            FontSize = 25
+        };
         public MainPage()
         {
-            InitializeComponent();
+            //InitializeComponent();
+            RelativeLayout relativeLayout = new RelativeLayout();
+
+            string BoxValueString = BoxValue.ToString();
+            label.Text = BoxValueString;
+            
+            Button button = new Button 
+            { 
+                Text= "Click on me",
+                FontSize = 16, 
+                BackgroundColor= Color.Red,
+            };
+            button.Clicked += OnButtonClicked;
+
+            relativeLayout.Children.Add(label,
+                Constraint.RelativeToParent((parent) =>
+                {
+                    return parent.Width * 0.5 - 50;    // установка координаты X
+                }),
+                Constraint.RelativeToParent((parent) =>
+                {
+                    return parent.Height * 0.5 - 150;   // установка координаты Y
+                })
+            );
+
+            relativeLayout.Children.Add(button,
+                Constraint.RelativeToView(label, (parent, view) =>
+                {
+                    return label.X - 30;    // установка координаты X
+                }),
+                Constraint.RelativeToView(label, (parent, view) =>
+                {
+                    return label.Y + 30;    // установка координаты Y
+                }),
+                Constraint.Constant(150), // установка ширины
+                Constraint.Constant(100) // установка высоты
+            );
+
+
+            Content = relativeLayout;
+        }
+
+        private void OnButtonClicked(object sender, EventArgs e)
+        {
+            BoxValue += 1;
+            string BoxValueString = BoxValue.ToString();
+            label.Text = BoxValueString;
         }
     }
-}
+}   
---
+++ Lanfi/Lanfi/StartPage.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace Lanfi
+{
+    internal class StartPage : ContentPage
+    {
+        public StartPage()
+        {
+            Label header = new Label()
+            {
+                Text = "It`s StartPage"
+            };
+            this.Content = header;
+        }
+    }
+}
